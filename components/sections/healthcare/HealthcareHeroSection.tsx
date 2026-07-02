20a84e10650569cc144ab5a4fc64dc55bb02654e
hamzabukhari-eforte <hamza@eforte.net>
2026-07-02 13:55:36 +0500

    Enhance HealthcareHeroSection by importing Link component for improved navigation functionality.

--- components/sections/healthcare/HealthcareHeroSection.tsx
+++ components/sections/healthcare/HealthcareHeroSection.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import ContactCTA from "@/components/atoms/ContactCTA";
 import { AnimatePresence, motion } from "framer-motion";
 import { HiArrowRight } from "react-icons/hi";
